f6c36220863ee11472054bdf573bb6a719c2a495
michihodges <michihodges@gmail.com>
2022-05-09 21:47:20 +0200

    del: Comments

--- pixel-art-maker/script.js
+++ pixel-art-maker/script.js
@@ -1,42 +1,33 @@
-// GLOBAL VARIABLES
-const color = document.querySelector('#colorPicker'); // select color input
-const submit = document.querySelector('#sizePicker'); // select size input
+const color = document.querySelector('#colorPicker');
+const submit = document.querySelector('#sizePicker');
 
-// CREATE GRID
 function makeGrid() {
-    // LOCAL VARIABLES
-    const canvas = document.querySelector('#pixelCanvas'); // select table id pixelCanvas
-    let height = document.querySelector('#inputHeight').value; // select value from input inputHeight
-    let width = document.querySelector('#inputWidth').value; // select value from input inputWidth
+    const canvas = document.querySelector('#pixelCanvas');
+    let height = document.querySelector('#inputHeight').value;
+    let width = document.querySelector('#inputWidth').value;
 
-    // RESET GRID
-    canvas.innerHTML = ''; // without resetting innerHTML to an empty string, submitted input values will add further grids
+    canvas.innerHTML = '';
 
-    // BUILD GRID
-    for (let row = 0; row < height; row++) { // loop over submitted input value height
-        const tableRow = document.createElement('tr'); // create appropriate number of tr elements
-        for (let col = 0; col < width; col++) { // loop over value of width input
-            const tableData = document.createElement('td'); // create appropriate number of td elements
-            tableRow.appendChild(tableData); // add tds
+    for (let row = 0; row < height; row++) {
+        const tableRow = document.createElement('tr');
+        for (let col = 0; col < width; col++) {
+            const tableData = document.createElement('td');
+            tableRow.appendChild(tableData);
         }
-        canvas.appendChild(tableRow); // adds trs
+        canvas.appendChild(tableRow);
     }
 
-    // EVENT LISTENER
-    canvas.addEventListener('click', respondToTheClick); // 1 event listener for all grid elements
+    canvas.addEventListener('click', respondToTheClick);
 
-    // EVENT DELEGATION
     function respondToTheClick(e) {
-        if (e.target.nodeName === 'TD') { // specifically target each td element for event listener to fire
-            console.log('A td element was clicked.'); // check that only a td element has been clicked
-            e.target.style.backgroundColor = color.value; // change target style background color
+        if (e.target.nodeName === 'TD') {
+            console.log('A td element was clicked.');
+            e.target.style.backgroundColor = color.value;
         }
     }
 }
 
-// SUBMIT
-// When size is submitted by the user, call makeGrid()
 submit.addEventListener('submit', function(e) {
-    e.preventDefault(); // presents grid
+    e.preventDefault();
     makeGrid();
 });
