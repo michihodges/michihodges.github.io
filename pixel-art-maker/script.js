1873779f0d886b4de46bd45dda2a27f064512c31
michihodges <michihodges@gmail.com>
2022-05-11 17:33:45 +0200

    feat: IIFE

--- pixel-art-maker/script.js
+++ pixel-art-maker/script.js
@@ -1,33 +1,35 @@
-const color = document.querySelector('#colorPicker');
-const submit = document.querySelector('#sizePicker');
+(function () {
+    const color = document.querySelector('#colorPicker');
+    const submit = document.querySelector('#sizePicker');
 
-function makeGrid() {
-    const canvas = document.querySelector('#pixelCanvas');
-    let height = document.querySelector('#inputHeight').value;
-    let width = document.querySelector('#inputWidth').value;
+    function makeGrid() {
+        const canvas = document.querySelector('#pixelCanvas');
+        let height = document.querySelector('#inputHeight').value;
+        let width = document.querySelector('#inputWidth').value;
 
-    canvas.innerHTML = '';
+        canvas.innerHTML = '';
 
-    for (let row = 0; row < height; row++) {
-        const tableRow = document.createElement('tr');
-        for (let col = 0; col < width; col++) {
-            const tableData = document.createElement('td');
-            tableRow.appendChild(tableData);
+        for (let row = 0; row < height; row++) {
+            const tableRow = document.createElement('tr');
+            for (let col = 0; col < width; col++) {
+                const tableData = document.createElement('td');
+                tableRow.appendChild(tableData);
+            }
+            canvas.appendChild(tableRow);
         }
-        canvas.appendChild(tableRow);
+
+        canvas.addEventListener('click', respondToTheClick);
+
+        function respondToTheClick(e) {
+            if (e.target.nodeName === 'TD') {
+                console.log('A td element was clicked.');
+                e.target.style.backgroundColor = color.value;
+            }
+        }
     }
 
-    canvas.addEventListener('click', respondToTheClick);
-
-    function respondToTheClick(e) {
-        if (e.target.nodeName === 'TD') {
-            console.log('A td element was clicked.');
-            e.target.style.backgroundColor = color.value;
-        }
-    }
-}
-
-submit.addEventListener('submit', function(e) {
-    e.preventDefault();
-    makeGrid();
-});
+    submit.addEventListener('submit', function(e) {
+        e.preventDefault();
+        makeGrid();
+    });
+}());
